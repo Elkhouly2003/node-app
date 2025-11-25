ec756978e938654daf520dfde8777789db677c75
Mohamed Emad Elkhouly <mohamed3mad235@gmail.com>
2025-11-25 06:10:30 +0200

    Create Node Js Project With MongoDp

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT;
+const app = express();
+
+//connect to data base
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = `mongo`;
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log("Connected"))
+  .catch((err) => {
+    console.log("Failed To Connect To Database", err);
+  });
+
+app.get("/", (req, res) => res.send("<h1> Hello World By Omda </h1>"));
+
+app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
